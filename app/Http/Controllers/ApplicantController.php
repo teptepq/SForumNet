481d0c43bf607e093002fbd2a65b181ea9710c30
VIN <115197877+Binn12345@users.noreply.github.com>
2024-05-30 18:10:45 +0800

    temporary push

---
+++ app/Http/Controllers/ApplicantController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\applicant;
+use Illuminate\Http\Request;
+
+class ApplicantController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(applicant $applicant)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(applicant $applicant)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, applicant $applicant)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(applicant $applicant)
+    {
+        //
+    }
+}
